951b3152c1fda79b837c8299f9b11bee7a9a7e08
Yashima-san <liyashima18@gmail.com>
2024-12-10 17:07:07 +0700

    Итогова версия

--- MyFile/Views/MainWindow.axaml.cs
+++ MyFile/Views/MainWindow.axaml.cs
@@ -14,7 +14,7 @@ public partial class MainWindow : Window
     {
         var input1 = LoginTextBox.Text; var input2 = PasswordTextBox.Text;
         //проверка логина и пароля
-        if (input1 == "inspector" && input1 == input2)
+        if (input1 == "n" && input1 == input2)
         {
             StatusMessage.Text = "Успешно!"; StatusMessage.Foreground = new Avalonia.Media.SolidColorBrush(Avalonia.Media.Colors.Green); //смена цвета на зеленый
             await Task.Delay(2000);
